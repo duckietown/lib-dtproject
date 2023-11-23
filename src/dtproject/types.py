3982d5bc48822291b36032e044b1572d73325b1b
tuxliri <dvde.iafrate98@gmail.com>
2023-11-23 15:27:16 +0100

    Fixing default `environment` field

--- src/dtproject/types.py
+++ src/dtproject/types.py
@@ -130,7 +130,7 @@ class ContainerConfiguration(dict):
     """
     __extends__: Optional[List[str]] = None
     __plain__: Optional[bool] = None
-    environment: Optional[Dict[str, str]] = None
+    environment: Optional[Dict[str, str]] = dataclasses.field(default_factory=dict)
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
